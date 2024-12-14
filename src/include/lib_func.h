51eb985f12fc0bc311d66ab7daa85b004f7886a0
imagire <imagire@gmail.com>
2024-12-15 00:14:01 +0900

    add files

---
+++ src/include/lib_func.h
@@ -0,0 +1,33 @@
+﻿#pragma once // インクルードガード
+
+#include <stdbool.h>
+
+// C++ でCのライブラリを使うときのおまじない
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	typedef struct item_
+	{
+		int key;
+		char value[256];
+	}item;
+
+	// バブルソート(引数が不適切であればfalseを返す)
+	bool bubble_sort(item *begin, const item* end);
+
+	// +-------------+
+	// |             |
+	// |             |
+	// +-----+-------+ <- begin
+	// | key | value |
+	// +-----+-------+
+	// | key | value |
+	// +-----+-------+
+	// | key | value |
+	// +-----+-------+ <- end
+
+	// C++ でCのライブラリを使うときのおまじない
+#ifdef __cplusplus
+} // extern "C"
+#endif
